b70b14157dd255d91114d00794c3895191c491c7
Ahadx488 <bytewise58@gmail.com>
2026-02-05 23:43:00 +0530

    Add files via upload

---
+++ basics/17_filter.js
@@ -0,0 +1,47 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+  console.log(userBooks);
+
+//   ===`forEach()`=== is used only to iterate over an array and perform actions on each element, but it does **not return a new array**, so assigning its result to a variable gives `undefined`. In contrast, ===`filter()`=== is used to **select elements based on a condition** and always returns a **new array** containing only the elements that satisfy the condition. This makes `filter()` ideal for extracting subsets of data, such as numbers greater than a value or objects matching multiple criteria (e.g., books of a specific genre published after a certain year). While the same filtering logic can be implemented using `forEach()` with manual pushing into a new array, `filter()` is cleaner, more readable, and purpose-built for this task.
